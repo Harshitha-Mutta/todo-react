4b6514e96c6dd637dfa221738ebf74bfa91a5703
Harshitha-Mutta <112391358+Harshitha-Mutta@users.noreply.github.com>
2023-01-30 09:35:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
-
 function App() {
+
+  
+  const [items,setItems]=useState([]);
+  const [newItem,setNewItem]=useState("");
+
+  function addItem(){
+
+    if(!newItem){
+      alert("Enter an Item");
+      return;
+    }
+    const item={
+      id:Math.floor(Math.random()*1000),
+      value: newItem
+    }
+    setItems(oldList=>[...oldList,item]);
+    setNewItem("");
+  }
+
+  function deleteItem(id){
+    const newArray =items.filter(item=>item.id !==id);
+    setItems(newArray);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+    <h1>Todo List</h1>
+
+    <input 
+    type="text" 
+    placeholder="Add an Item"
+    value={newItem}
+    onChange={e=>setNewItem(e.target.value)}
+    />
+
+    <button onClick={()=>addItem()}>Add</button>
+
+    <ul>
+     {items.map(item=>{
+      return(
+        <li key={item.key}>{item.value}   <button className="delete-button" onClick={()=> deleteItem(item.id)}>❌</button></li> 
+      )
+     })}
+    </ul>
+
+
+   </div>
+   
   );
 }
 
